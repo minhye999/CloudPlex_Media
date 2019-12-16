16fade76e9e13d356c1ca4050f652e7a70aefda0
9 <57486150+minhye99@users.noreply.github.com>
2019-12-16 16:38:17 +0900

    Welcome 페이지 표준화 완료

--- staging/Chrome/CORE/Welcome.py
+++ staging/Chrome/CORE/Welcome.py
@@ -20,27 +20,31 @@ class Welcome(unittest.TestCase):
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
-    # Web page Title 확인
+    # Web page Browser Title 확인
     def test_check_title(self):
         driver = self.driver
         self.driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
         try:
+            # Web page Browser Title 출력 확인
             self.assertEqual("CloudPlex Media", driver.title)
         except:
             print('TEST FAIL : check_title')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
             now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             self.driver.save_screenshot('F:/PyCharm/Project/CloudPlex_Media/staging/Test_Results/Screenshots/test_Title-%s.png' % now)
         else:
             print('TEST PASS : check_title')
 
-    # Logo 확인
+    # Brand Logo 확인
     def test_check_logo(self):
         driver = self.driver
         self.driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
         try:
+            # Brand Logo 출력 확인
             self.driver.find_element_by_class_name("brand-logo")
         except:
             print('TEST FAIL : check_logo')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
             now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             self.driver.save_screenshot('F:/PyCharm/Project/CloudPlex_Media/staging/Test_Results/Screenshots/test_Title-%s.png' % now)
         else:
@@ -51,24 +55,49 @@ class Welcome(unittest.TestCase):
         driver = self.driver
         self.driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
         try:
+            # Welcome Text 출력 확인
             self.assertEqual("Welcome!", driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='You need to sign in a Megazone Accounts account to get started.'])[1]/preceding::h4[1]").text)
         except:
             print('TEST FAIL : check_welcome')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
             now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             self.driver.save_screenshot('F:/PyCharm/Project/CloudPlex_Media/staging/Test_Results/Screenshots/test_Welcome-%s.png' % now)
         else:
             print('TEST PASS : check_welcome')
 
-    # Sign in Button 확인
+    # Sign in Button 출력 및 이동 확인
     def test_check_btn_signIn(self):
         driver = self.driver
         driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
         try:
+            # [Sign in]버튼 확인
             self.assertEqual("Sign in with Megazone Accounts",
                              driver.find_element_by_link_text("Sign in with Megazone Accounts").text)
+            # [Sign in]버튼 클릭
+            self.driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div/div/div[2]/form/div[1]/a').click()
+            # ID 입력
+            self.driver.find_element_by_xpath('//*[@id="username"]').send_keys('mcmtestowner@gmail.com')
+            # [다음]버튼 클릭
+            self.driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div/div/div[2]/div[4]/button').click()
+            time.sleep(3)
+            # PW 입력
+            self.driver.find_element_by_xpath('//*[@id="password"]').send_keys('Megazone1!')
+            # [로그인]버튼 클릭
+            self.driver.find_element_by_xpath(
+                u"(.//*[normalize-space(text()) and normalize-space(.)='로그인'])[1]/following::strong[1]").click()
+            # 로그인되었는지 확인 (Stage / Project / Name / Email)
+            time.sleep(3)
+            self.assertEqual("Megazone (Owner)", driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='mcmtestowner@gmail.com'])[1]/preceding::em[1]").text)
+            self.assertEqual("mcmtestowner@gmail.com", driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Megazone (Owner)'])[1]/following::small[1]").text)
+            self.assertEqual("CloudPlex Media", driver.title)
+            self.assertEqual("Hi, Megazone (Owner)", driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='mcmtestowner@gmail.com'])[1]/following::h1[1]").text)
         except:
             print('TEST FAIL : check_btn_signIn')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
             now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             self.driver.save_screenshot('F:/PyCharm/Project/CloudPlex_Media/staging/Test_Results/Screenshots/test_Sign-%s.png' % now)
         else:
@@ -79,9 +108,15 @@ class Welcome(unittest.TestCase):
         driver = self.driver
         driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
         try:
+            # [Create Account]버튼 확인
             self.assertEqual("Create account", driver.find_element_by_link_text("Create account").text)
+            # [Create Account]버튼 클릭
+            self.driver.find_element_by_link_text("Create account").click()
+            # 이동한 페이지 확인 (Title)
+            # 이슈있어 확인불가
         except:
             print('TEST FAIL : check_link_createAccount')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
             now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             self.driver.save_screenshot('F:/PyCharm/Project/CloudPlex_Media/staging/Test_Results/Screenshots/test_CreateAccount-%s.png' % now)
         else:
