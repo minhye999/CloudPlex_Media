b1af7205f274cdbcf5a95640a625fe20f1ad5c78
9 <57486150+minhye99@users.noreply.github.com>
2020-01-07 20:52:17 +0900

    PopUp_Publish - Create

--- staging/Chrome/CORE/PopUp_Publish.py
+++ staging/Chrome/CORE/PopUp_Publish.py
@@ -19,7 +19,7 @@ class PopUp_Publish(unittest.TestCase):
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
-    def test_check_title(self):  # Publish 기능 확인
+    def test_check_title(self):  # 팝업 Title 확인
         driver = self.driver
         driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
         try:
@@ -44,6 +44,37 @@ class PopUp_Publish(unittest.TestCase):
             # 팝업 Title 확인
             self.assertEqual("Publish", driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='Channels'])[1]/following::h5[1]").text)
+        except:
+            print('TEST FAIL : test_check_title')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
+            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.save_screenshot(
+                '../../../staging/Chrome/Test_Results/Screenshots/test_check_title-%s.png' % now)
+        else:
+            print('TEST PASS : test_check_title')
+
+    def test_check_close(self):  # 팝업 닫기 기능 확인
+        driver = self.driver
+        driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
+        try:
+            staging.Chrome.CORE.common.move_main(self)  # Project Main page로 이동하는 공통 모듈 호출
+            # Videos 메뉴로 이동
+            driver.find_element_by_link_text("Videos").click()
+            time.sleep(3)
+            # DASH Video 상세 페이지로 이동 (ID : 1578039655b2n5)
+            driver.find_element_by_id("quick-search").click()
+            driver.find_element_by_id("quick-search").clear()
+            driver.find_element_by_id("quick-search").send_keys("1578039655b2n5")
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Quick Search'])[1]/following::span[1]").click()
+            time.sleep(3)
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Refresh'])[1]/following::p[1]").click()
+            time.sleep(3)
+            # [Publish]버튼 클릭
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Videos'])[2]/following::button[1]").click()
+            time.sleep(3)
             # [X]버튼 확인
             driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div/div/div/div/button/span/i").click()
             # [X]버튼 클릭
@@ -52,11 +83,73 @@ class PopUp_Publish(unittest.TestCase):
             # [Publish]버튼 클릭
             driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='Videos'])[2]/following::span[1]").click()
+        except:
+            print('TEST FAIL : test_check_close')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
+            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.save_screenshot(
+                '../../../staging/Chrome/Test_Results/Screenshots/test_check_close-%s.png' % now)
+        else:
+            print('TEST PASS : test_check_close')
+
+    def test_check_video(self):  # Video Name, ID 확인
+        driver = self.driver
+        driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
+        try:
+            staging.Chrome.CORE.common.move_main(self)  # Project Main page로 이동하는 공통 모듈 호출
+            # Videos 메뉴로 이동
+            driver.find_element_by_link_text("Videos").click()
+            time.sleep(3)
+            # DASH Video 상세 페이지로 이동 (ID : 1578039655b2n5)
+            driver.find_element_by_id("quick-search").click()
+            driver.find_element_by_id("quick-search").clear()
+            driver.find_element_by_id("quick-search").send_keys("1578039655b2n5")
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Quick Search'])[1]/following::span[1]").click()
+            time.sleep(3)
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Refresh'])[1]/following::p[1]").click()
+            time.sleep(3)
+            # [Publish]버튼 클릭
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Videos'])[2]/following::button[1]").click()
+            time.sleep(3)
             # Video 항목명, Data 확인
             self.assertEqual("Video", driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='Publish'])[1]/following::h6[1]").text)
             self.assertEqual(u"자동화 테스트 - 001(1578039655b2n5)", driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='Video'])[1]/following::p[1]").text)
+        except:
+            print('TEST FAIL : test_check_video')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
+            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.save_screenshot(
+                '../../../staging/Chrome/Test_Results/Screenshots/test_check_video-%s.png' % now)
+        else:
+            print('TEST PASS : test_check_video')
+
+    def test_check_toPublish(self):  # to Publish 출력 및 Publish 기능 확인
+        driver = self.driver
+        driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
+        try:
+            staging.Chrome.CORE.common.move_main(self)  # Project Main page로 이동하는 공통 모듈 호출
+            # Videos 메뉴로 이동
+            driver.find_element_by_link_text("Videos").click()
+            time.sleep(3)
+            # DASH Video 상세 페이지로 이동 (ID : 1578039655b2n5)
+            driver.find_element_by_id("quick-search").click()
+            driver.find_element_by_id("quick-search").clear()
+            driver.find_element_by_id("quick-search").send_keys("1578039655b2n5")
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Quick Search'])[1]/following::span[1]").click()
+            time.sleep(3)
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Refresh'])[1]/following::p[1]").click()
+            time.sleep(3)
+            # [Publish]버튼 클릭
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Videos'])[2]/following::button[1]").click()
+            time.sleep(3)
             # to Publish 항목명, Data 확인
             self.assertEqual("to Publish", driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='(1578039655b2n5)'])[1]/following::h6[1]").text)
@@ -70,6 +163,37 @@ class PopUp_Publish(unittest.TestCase):
             time.sleep(3)
             # 배포 확인
             self.driver.find_element_by_xpath("//div/span/i[@class=':after']") #문제로다
+        except:
+            print('TEST FAIL : test_check_toPublish')
+            logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
+            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.save_screenshot(
+                '../../../staging/Chrome/Test_Results/Screenshots/test_check_toPublish-%s.png' % now)
+        else:
+            print('TEST PASS : test_check_toPublish')
+
+    def test_check_embedCode(self):  # Embed code 출력 및 Copy 기능 확인
+        driver = self.driver
+        driver.get("http://mz-cm-console-stg-stage.s3-website.ap-northeast-2.amazonaws.com/welcome")
+        try:
+            staging.Chrome.CORE.common.move_main(self)  # Project Main page로 이동하는 공통 모듈 호출
+            # Videos 메뉴로 이동
+            driver.find_element_by_link_text("Videos").click()
+            time.sleep(3)
+            # DASH Video 상세 페이지로 이동 (ID : 1578039655b2n5)
+            driver.find_element_by_id("quick-search").click()
+            driver.find_element_by_id("quick-search").clear()
+            driver.find_element_by_id("quick-search").send_keys("1578039655b2n5")
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Quick Search'])[1]/following::span[1]").click()
+            time.sleep(3)
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Refresh'])[1]/following::p[1]").click()
+            time.sleep(3)
+            # [Publish]버튼 클릭
+            driver.find_element_by_xpath(
+                "(.//*[normalize-space(text()) and normalize-space(.)='Videos'])[2]/following::button[1]").click()
+            time.sleep(3)
             # Embed Code 항목명, Data 확인
             self.assertEqual("Embed Code", driver.find_element_by_xpath(
                 "(.//*[normalize-space(text()) and normalize-space(.)='Instagram'])[1]/following::h6[1]").text)
@@ -84,13 +208,13 @@ class PopUp_Publish(unittest.TestCase):
                 "//div[@id='root']/div/div/div/div/div/div/div/div/div[2]/div[3]/button/i").click()
             # Paste를 어떻게 확인할지 생각해보자
         except:
-            print('TEST FAIL : test_check_title')
+            print('TEST FAIL : test_check_embedCode')
             logging.basicConfig(stream=sys.stderr, level=logging.error)  # 로그 출력
             now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             self.driver.save_screenshot(
-                '../../../staging/Chrome/Test_Results/Screenshots/test_check_title-%s.png' % now)
+                '../../../staging/Chrome/Test_Results/Screenshots/test_check_embedCode-%s.png' % now)
         else:
-            print('TEST PASS : test_check_title')
+            print('TEST PASS : test_check_embedCode')
 
     def tearDown(self):
         self.driver.close()
@@ -100,9 +224,10 @@ class PopUp_Publish(unittest.TestCase):
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(PopUp_Publish('test_check_title'))
-    suite.addTest(PopUp_Publish("test_check_viewMode"))
-    suite.addTest(PopUp_Publish("test_check_btn_folding"))
-    suite.addTest(PopUp_Publish("test_check_btn_close"))
+    suite.addTest(PopUp_Publish("test_check_close"))
+    suite.addTest(PopUp_Publish("test_check_video"))
+    suite.addTest(PopUp_Publish("test_check_toPublish")) # Publish 완료 확인하는 방법
+    suite.addTest(PopUp_Publish("test_check_embedCode")) # Copy한 Code를 Paste 어떻게 확인할지 생각해보자
     return suite
 
 if __name__ == "__main__":
